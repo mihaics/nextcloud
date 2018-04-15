314c9b80e1fa37068c8504daa4c114a0f63ce4d3
Mihai Csaky <mihai.csaky@gmail.com>
2018-04-15 10:53:01 +0300

    sudo to nginx

--- start.sh
+++ start.sh
@@ -41,41 +41,41 @@ fi
 
 if [ -n "$OVERWRITEHOST" ]
 then
-  $ocdir/occ config:system:set overwritehost --value=$OVERWRITEHOST
+  sudo -u nginx $ocdir/occ config:system:set overwritehost --value=$OVERWRITEHOST
 fi
 
 if [ -n "$OVERWRITEPROTOCOL" ]
 then
-  $ocdir/occ config:system:set overwriteprotocol --value=$OVERWRITEPROTOCOL
+  sudo -u nginx $ocdir/occ config:system:set overwriteprotocol --value=$OVERWRITEPROTOCOL
 fi
 
 if [ -n "$WEB_ROOT" ]
 then
-  $ocdir/occ config:system:set overwritewebroot --value=$WEB_ROOT
+  sudo -u nginx $ocdir/occ config:system:set overwritewebroot --value=$WEB_ROOT
 fi
 
 if [ -n "$OVERWRITECONDADDR" ]
 then
-  $ocdir/occ config:system:set overwritecondaddr --value=$OVERWRITECONDADDR
+  sudo -u nginx $ocdir/occ config:system:set overwritecondaddr --value=$OVERWRITECONDADDR
 fi
 
 
 if [ -n "$TRUSTED_DOMAIN1" ]
 then
-  $ocdir/occ config:system:set trusted_domains 1 --value=$TRUSTED_DOMAIN1
+  sudo -u nginx $ocdir/occ config:system:set trusted_domains 1 --value=$TRUSTED_DOMAIN1
 fi
 
 
 
 if [ -n "$TRUSTED_DOMAIN2" ]
 then
-  $ocdir/occ config:system:set trusted_domains 2 --value=$TRUSTED_DOMAIN2
+  sudo -u nginx $ocdir/occ config:system:set trusted_domains 2 --value=$TRUSTED_DOMAIN2
 fi
 
 
 if [ -n "$EXTERNAL_DOMAIN" ]
 then
-  $ocdir/occ config:system:set overwrite.cli.url --value=$EXTERNAL_DOMAIN
+  sudo -u nginx $ocdir/occ config:system:set overwrite.cli.url --value=$EXTERNAL_DOMAIN
 fi
 
 
